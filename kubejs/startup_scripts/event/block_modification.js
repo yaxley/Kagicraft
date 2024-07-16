cee900c94591c49603476cd95482f205ea176c7d
yaxley <timotheusoverbeek@gmail.com>
2024-07-16 16:51:41 +0200

    Updated KubeJS to 1.20

---
+++ kubejs/startup_scripts/event/block_modification.js
@@ -0,0 +1,119 @@
+BlockEvents.modification(event =>
+{
+   event.modify('minecraft:deepslate_coal_ore', block => 
+  {
+    block.destroySpeed = 9
+  })
+  event.modify('minecraft:deepslate_iron_ore', block => 
+  {
+    block.destroySpeed = 9
+  })
+  event.modify('minecraft:deepslate_copper_ore', block => 
+  {
+    block.destroySpeed = 9
+  })
+  event.modify('minecraft:deepslate_gold_ore', block => 
+  {
+    block.destroySpeed = 9
+  })
+  event.modify('minecraft:deepslate_redstone_ore', block => 
+  {
+    block.destroySpeed = 9
+  })
+  event.modify('minecraft:deepslate_emerald_ore', block => 
+  {
+    block.destroySpeed = 9
+  })
+  event.modify('minecraft:deepslate_lapis_ore', block => 
+  {
+    block.destroySpeed = 9
+  })
+  event.modify('minecraft:deepslate_diamond_ore', block => 
+  {
+    block.destroySpeed = 9
+  })
+  event.modify('thermal:deepslate_tin_ore', block => 
+  {
+    block.destroySpeed = 9
+  })
+  event.modify('thermal:deepslate_lead_ore', block => 
+  {
+    block.destroySpeed = 9
+  })
+  event.modify('thermal:deepslate_silver_ore', block => 
+  {
+    block.destroySpeed = 9
+  })
+  event.modify('thermal:deepslate_sapphire_ore', block => 
+  {
+    block.destroySpeed = 9
+  })
+  event.modify('thermal:deepslate_ore_nickel', block => 
+  {
+    block.destroySpeed = 9
+  })
+  event.modify('minecraft:cobbled_deepslate', block => 
+  {
+    block.destroySpeed = 7.5
+  })
+  event.modify('minecraft:cobbled_deepslate_stairs', block => 
+  {
+    block.destroySpeed = 7.5
+  })
+  event.modify('minecraft:cobbled_deepslate_slab', block => 
+  {
+    block.destroySpeed = 7.5
+  })
+   event.modify('minecraft:polished_deepslate', block => 
+  {
+    block.destroySpeed = 7.5
+  })
+  event.modify('minecraft:polished_deepslate_stairs', block => 
+  {
+    block.destroySpeed = 7.5
+  })
+  event.modify('minecraft:polished_deepslate_slab', block => 
+  {
+    block.destroySpeed = 7.5
+  })
+  event.modify('minecraft:deepslate_bricks', block => 
+  {
+    block.destroySpeed = 7.5
+  })
+  event.modify('minecraft:cracked_deepslate_bricks', block => 
+  {
+    block.destroySpeed = 7.5
+  })
+  event.modify('minecraft:deepslate_brick_stairs', block => 
+  {
+    block.destroySpeed = 7.5
+  })
+  event.modify('minecraft:deepslate_brick_slab', block => 
+  {
+    block.destroySpeed = 7.5
+  })
+   event.modify('minecraft:deepslate_tiles', block => 
+  {
+    block.destroySpeed = 7.5
+  })
+  event.modify('minecraft:cracked_deepslate_tiles', block => 
+  {
+    block.destroySpeed = 7.5
+  })
+  event.modify('minecraft:deepslate_tile_stairs', block => 
+  {
+    block.destroySpeed = 7.5
+  })
+  event.modify('minecraft:deepslate_tile_slab', block => 
+  {
+    block.destroySpeed = 7.5
+  })
+  event.modify('minecraft:deepslate', block => 
+  {
+    block.destroySpeed = 6.75
+  })
+  event.modify('minecraft:infested_deepslate', block => 
+  {
+    block.destroySpeed = 6.75
+  })
+})
---
+++ kubejs/startup_scripts/event/custom_items.js
@@ -0,0 +1,10 @@
+StartupEvents.registry('item', event => {
+  event.create('copper_coin')
+  event.create('silver_coin')
+  event.create('gold_coin')
+  event.create('blood_emerald')
+  event.create('tin_plate')
+  event.create('quicksilver_plate')
+  event.create('diamond_plate')
+  event.create('manasteel_plate')
+})
